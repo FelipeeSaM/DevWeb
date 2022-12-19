7609e4ca8d0b9ec4bdc529c0c3fde2c8663ce149
eu mesmo <luiz_sena@hotmail.com>
2022-12-19 02:10:23 -0300

    Terminando form

--- Exercicios/form/server.js
+++ Exercicios/form/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); // SERVER.JS é o servidor que faz com que a aplicação fique online (basta deixar o pc ligado com o node server.js rodando), posso acessar de qualquer navegador
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -6,7 +6,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Foi</h1>')
+    resp.send('<h1>Usuario cadastrado</h1>')
 });
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Usuario alterado</h1>')
+});
+
 
 app.listen(3003);
