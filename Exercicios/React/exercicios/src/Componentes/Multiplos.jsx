d5746365b2db6ca40f596619d0cc510e3ff7745e
eu mesmo <luiz_sena@hotmail.com>
2023-01-16 11:23:34 -0300

    Multiplos

---
+++ Exercicios/React/exercicios/src/Componentes/Multiplos.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//2°.1° ex:
+// export const BoaTarde = props => <h1>Boa tarde, {props.nome}!</h1>
+// export const BoaNoite = props => <h1>Boa noite, {props.nome}!</h1>
+
+//2°.2° ex:
+// export const BoaTarde = props => <h1>Boa tarde, {props.nome}!</h1>
+// export const BoaNoite = props => <h1>Boa noite, {props.nome}!</h1>
+// export default BoaTarde
+
+//2°.3° ex:
+export const BoaTarde = props => <h1>Boa tarde, {props.nome}!</h1>
+export const BoaNoite = props => <h1>Boa noite, {props.nome}!</h1>
+export default { BoaTarde, BoaNoite }
--- Exercicios/React/exercicios/src/index.js
+++ Exercicios/React/exercicios/src/index.js
@@ -1,6 +1,41 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-import BomDia from './Componentes/BomDia'
+// 1° ex:
+// import BomDia from './Componentes/BomDia'
 
-ReactDom.render(<BomDia nome={'Zoio'} />, document.getElementById('root'))
+// ReactDom.render(<BomDia nome={'Zoio'} />, document.getElementById('root'))
+
+
+// 2°.1° ex:
+// import { BoaTarde, BoaNoite } from './Componentes/Multiplos'
+
+// ReactDom.render(
+//     <div>
+//         <BoaTarde nome="ana" />
+//         <BoaNoite nome="bia" />
+//     </div>,
+//     document.getElementById('root')
+// )
+
+// 2°.2° ex: export default permite que o componente seja exportado sem a necessidade de desconstruir o array.
+// import BoaTarde, { BoaNoite } from './Componentes/Multiplos'
+
+// ReactDom.render(
+//     <div>
+//         <BoaTarde nome="ana" />
+//         <BoaNoite nome="bia" />
+//     </div>,
+//     document.getElementById('root')
+// )
+
+// 2°.3° ex: Posso dar o nome que quiser para o import e usá-lo como objeto na chamada. Tem que ser no default
+import Multi from './Componentes/Multiplos'
+
+ReactDom.render(
+    <div>
+        <Multi.BoaTarde nome="ana" />
+        <Multi.BoaNoite nome="bia" />
+    </div>,
+    document.getElementById('root')
+)
